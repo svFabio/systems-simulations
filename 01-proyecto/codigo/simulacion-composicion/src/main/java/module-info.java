9d4a9f3ba0bc47c44dc766d15437129e846a6695
svFabio <svfabio.andres@gmail.com>
2026-09-01 19:04:09 -0400

    chore: update project to Java 21 with JFreeChart
    - Upgrade compiler from Java 11 to Java 21
    - Add javafx-swing for JFreeChart interop
    - Add JFreeChart 1.5.4 for histograms + curves
    - Update maven-compiler-plugin to 3.13.0
    - Update javafx-maven-plugin to 0.0.8
    - Add .gitignore for build artifacts

--- 01-proyecto/codigo/simulacion-composicion/src/main/java/module-info.java
+++ 01-proyecto/codigo/simulacion-composicion/src/main/java/module-info.java
@@ -1,4 +1,20 @@
-module com.universidad {
+module com.simulacion {
     requires javafx.controls;
-    exports com.universidad;
+    requires javafx.swing;
+    requires java.desktop;
+    requires org.jfree.jfreechart;
+
+    opens com.simulacion to javafx.controls;
+    opens com.simulacion.ejercicio1 to javafx.controls;
+    opens com.simulacion.ejercicio2 to javafx.controls;
+    opens com.simulacion.ejercicio3 to javafx.controls;
+    opens com.simulacion.ejercicio4 to javafx.controls;
+    opens com.simulacion.util to javafx.controls;
+
+    exports com.simulacion;
+    exports com.simulacion.ejercicio1;
+    exports com.simulacion.ejercicio2;
+    exports com.simulacion.ejercicio3;
+    exports com.simulacion.ejercicio4;
+    exports com.simulacion.util;
 }
